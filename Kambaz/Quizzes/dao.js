18524c462508a89b253787415dc5ae281ca61e2b
Ryan Saperstein <rmsaperstein@gmail.com>
2025-04-18 16:57:50 -0400

    Started quiz details

--- Kambaz/Quizzes/dao.js
+++ Kambaz/Quizzes/dao.js
@@ -1,23 +1,28 @@
 import Database from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
 export function findQuizzesForCourse(courseId) {
-    const { quizzes } = Database;
-    return quizzes.filter((quiz) => quiz.course === courseId);
+  const { quizzes } = Database;
+  return quizzes.filter((quiz) => quiz.course === courseId);
 }
 export function createQuiz(quiz) {
-    const newQuiz = { ...quiz, _id: uuidv4() };
-    Database.quizzes = [...Database.quizzes, newQuiz];
-    return newQuiz;
+  const newQuiz = { ...quiz, _id: uuidv4() };
+  Database.quizzes = [...Database.quizzes, newQuiz];
+  return newQuiz;
 }
 
 export function deleteQuiz(quizId) {
-    const { quizzes } = Database;
-    Database.quizzes = quizzes.filter((quiz) => quiz._id !== quizId);
+  const { quizzes } = Database;
+  Database.quizzes = quizzes.filter((quiz) => quiz._id !== quizId);
 }
 
 export function updateQuiz(quizId, quizUpdates) {
-    const { quizzes } = Database;
-    const quiz = quizzes.find((quiz) => quiz._id === quizId);
-    Object.assign(quiz, quizUpdates);
-    return quiz;
+  const { quizzes } = Database;
+  const quiz = quizzes.find((quiz) => quiz._id === quizId);
+  Object.assign(quiz, quizUpdates);
+  return quiz;
+}
+
+export function getQuiz(quizId) {
+  const { quizzes } = Database;
+  return quizzes.filter((quiz) => quiz._id === quizId)[0];
 }
--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-import express from 'express'
-import mongoose from 'mongoose';
-import Hello from "./Hello.js"
-import Lab5 from "./Lab5/index.js"
-import cors from "cors"
-import UserRoutes from './Kambaz/Users/routes.js'
-import CourseRoutes from './Kambaz/Courses/routes.js'
+// Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
+
+import express from "express";
+import mongoose from "mongoose";
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5/index.js";
+import cors from "cors";
+import UserRoutes from "./Kambaz/Users/routes.js";
+import CourseRoutes from "./Kambaz/Courses/routes.js";
 import "dotenv/config";
 import session from "express-session";
-import ModuleRoutes from './Kambaz/Modules/routes.js'
-import AssignmentRoutes from './Kambaz/Assignments/routes.js'
-import QuizRoutes from './Kambaz/Quizzes/routes.js';
-import EnrollmentRoutes from './Kambaz/Enrollments/routes.js'
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+import ModuleRoutes from "./Kambaz/Modules/routes.js";
+import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
+import QuizRoutes from "./Kambaz/Quizzes/routes.js";
+import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
 mongoose.connect(CONNECTION_STRING);
-const app = express()
+const app = express();
 app.use(
-    cors({
-        credentials: true,
-        origin: process.env.NETLIFY_URL || "http://localhost:5173",
-    })
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:5173",
+  })
 );
 const sessionOptions = {
-    secret: process.env.SESSION_SECRET || "kambaz",
-    resave: false,
-    saveUninitialized: false,
+  secret: process.env.SESSION_SECRET || "kambaz",
+  resave: false,
+  saveUninitialized: false,
 };
 if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-        sameSite: "none",
-        secure: true,
-        domain: process.env.NODE_SERVER_DOMAIN,
-    };
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
 }
 app.use(session(sessionOptions));
-app.use(express.json())
-UserRoutes(app)
-CourseRoutes(app)
-Lab5(app)
-Hello(app)
-ModuleRoutes(app)
-AssignmentRoutes(app)
-QuizRoutes(app)
-EnrollmentRoutes(app)
-app.listen(process.env.PORT || 4000)
-
+app.use(express.json());
+UserRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+Hello(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
+QuizRoutes(app);
+EnrollmentRoutes(app);
+app.listen(process.env.PORT || 4000);
